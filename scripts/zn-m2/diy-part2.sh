e63f3c31c173e9fc1c7defebdcd04371665933c4
Style <62116123+Style00@users.noreply.github.com>
2024-08-26 11:19:19 +0800

    Update diy-part2.sh

--- scripts/zn-m2/diy-part2.sh
+++ scripts/zn-m2/diy-part2.sh
@@ -10,9 +10,13 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 #优先安装 passwall 源
+
 rm -rf feeds/luci/applications/luci-app-mosdns
 rm -rf feeds/packages/net/{alist,adguardhome,mosdns,xray*,v2ray*,v2ray*,sing*,smartdns}
 rm -rf feeds/packages/utils/v2dat
 rm -rf feeds/packages/lang/golang
+rm -rf feeds/small/luci-app-homeproxy
 ./scripts/feeds install -a -f -p kenzo
+
 ./scripts/feeds install -a -f -p small
+rm -rf feeds/small/luci-app-homeproxy
